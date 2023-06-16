7262468de121f186f02f6ea38b40ba0ed30f7556
Mohamad Amirul Hakim Bin Johari <amirulhakim5383@gmail.com>
2023-06-17 00:15:58 +0800

    3 backend commit

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ try {
   console.error(error);
 }
 
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: "http://footcourse-backend-production.up.railway.app" }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(router);
