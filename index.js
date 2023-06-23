a04ec501a1f72f6105ed56d8dc8b378d31fd1995
Mohamad Amirul Hakim Bin Johari <76082504+amirukim@users.noreply.github.com>
2023-06-23 17:14:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -28,4 +28,4 @@ app.use(express.json());
 app.use(router);
 
 //app.listen(5000, () => console.log("Server Running at port 5000"));
-app.listen(port, "0.0.0.0", () => console.log("Server Running at port 5000"));
+app.listen(3000, "0.0.0.0", () => console.log("Server Running at port 5000"));
