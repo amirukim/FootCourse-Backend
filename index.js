8afd0bcb4a1ce45992156986d447e5fa5299e645
Mohamad Amirul Hakim Bin Johari <amirulhakim5383@gmail.com>
2023-06-25 03:22:18 +0800

    backend updated

--- index.js
+++ index.js
@@ -10,25 +10,25 @@ dotenv.config();
 
 const app = express();
 
-async function startServer() {
-  const port = process.env.PORT || 3306;
-  try {
-    await db.authenticate();
-    console.log("Database connected...");
-    //await db.sync();
-    //await Learns.sync();
-  } catch (error) {
-    console.error(error);
-  }
-
-  app.use(cors({ credentials: true, origin: "https://footcourse.netlify.app" }));
-  //app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
-  app.use(cookieParser());
-  app.use(express.json());
-  app.use(router);
-
-  //app.listen(5000, () => console.log("Server Running at port 5000"));
-  app.listen(port, "127.0.0.1", () => console.log("Server Running at port 5000"));
+//async function startServer() {
+const port = process.env.PORT || 5000;
+try {
+  await db.authenticate();
+  console.log("Database connected...");
+  //await db.sync();
+  //await Learns.sync();
+} catch (error) {
+  console.error(error);
 }
 
-startServer();
+app.use(cors({ credentials: true, origin: "https://footcourse.netlify.app" }));
+//app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cookieParser());
+app.use(express.json());
+app.use(router);
+
+//app.listen(5000, () => console.log("Server Running at port 5000"));
+app.listen(port, "0.0.0.0", () => console.log("Server Running at port 5000"));
+/*}
+
+startServer();*/
