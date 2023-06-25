16c3e51c3cdd603811d6f8162c1a011f78b4fe8d
Mohamad Amirul Hakim Bin Johari <76082504+amirukim@users.noreply.github.com>
2023-06-25 21:28:35 +0800

    Update Database.js

--- config/Database.js
+++ config/Database.js
@@ -2,15 +2,10 @@ import { Sequelize } from "sequelize";
 //require("dotenv").config();
 //require("dotenv").config({ path: "backend/.env" });
 
-const db = new Sequelize("footcourse", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-});
-
-/*const db = new Sequelize("railway", "root", "4JJwhRO1inHhma8H7Pm7", {
+const db = new Sequelize("railway", "root", "4JJwhRO1inHhma8H7Pm7", {
   host: "containers-us-west-7.railway.app",
   port: "7566",
   dialect: "mysql",
-});*/
+});
 
 export default db;
